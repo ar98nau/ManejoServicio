1fb849c1b08e241c0aacae587322de1b3970807c
ar98nau <anr3@alumnes.udl.cat>
2019-03-16 10:35:29 +0100

    A

--- app/src/main/java/com/android/mdw/demo/ElServicio.java
+++ app/src/main/java/com/android/mdw/demo/ElServicio.java
@@ -3,12 +3,14 @@ package com.android.mdw.demo;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.widget.Toast;
 
 public class ElServicio extends Service {
 
 	private MediaPlayer player;
+	private MediaPlayer player1;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -20,20 +22,31 @@ public class ElServicio extends Service {
 	public void onCreate() {
 		Toast.makeText(this, R.string.creaserv, Toast.LENGTH_LONG).show();		
 		player = MediaPlayer.create(this, R.raw.train);
+		player1 = MediaPlayer.create(this, R.raw.bob_marley_cybl);
 		player.setLooping(true);
+		player1.setLooping(true);
 	}
 
 	@Override
 	public void onDestroy() {
 		Toast.makeText(this, R.string.finaserv, Toast.LENGTH_LONG).show();
 		player.stop();
+		player1.stop();
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startid) {
 		Toast.makeText(this, R.string.iniserv, Toast.LENGTH_LONG).show();
-		player.start();
-		return startid;		
+
+		Bundle data = intent.getExtras();
+		String so = data.getString(getString(R.string.mess));
+
+		if (so.equals(getString(R.string.selectSonido))) {
+			player.start();
+		} else if (so.equals(getString(R.string.selectCancion))) {
+			player1.start();
+		}
+		return startid;
 	}	
 
 }
